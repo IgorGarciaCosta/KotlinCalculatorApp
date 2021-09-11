2392ea6f5da78483d77721a756e27d75be82b48f
IgorGarciaCosta <igonildo7@gmail.com>
2021-09-10 22:34:45 -0300

    Adding other operations and editing text position in text viewer

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -59,6 +58,39 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun removeZeroAfterDot(result: String):String{
+        var value = result
+        if(result.contains(".0")){
+            value = result.substring(0, result.length-2)
+        }
+        return value
+
+    }
+
+    fun dealWithOperator(tvValue: String, prefix: String, signal:String) {
+
+            val splitValue = tvValue.split(signal)
+            var one = splitValue[0]
+            var two = splitValue[1]
+
+            if(!prefix.isEmpty()){
+                one = prefix+one
+            }
+            if (signal=="-"){
+                tvInput.text = removeZeroAfterDot((one.toDouble()-two.toDouble()).toString())
+            }
+            if (signal=="+"){
+                tvInput.text = removeZeroAfterDot((one.toDouble()+two.toDouble()).toString())
+            }
+            if (signal=="*"){
+                tvInput.text = removeZeroAfterDot((one.toDouble()*two.toDouble()).toString())
+            }
+            if (signal=="/"){
+                tvInput.text = removeZeroAfterDot((one.toDouble()/two.toDouble()).toString())
+            }
+
+    }
+
     fun onEqual(view: View){
         if(lastNumeric){
             var tvValue = tvInput.text.toString()
@@ -68,16 +100,21 @@ class MainActivity : AppCompatActivity() {
                     prefix = "-"
                     tvValue = tvValue.substring(1)
                 }
-                if(tvValue.contains("-")){
-                    val splitValue = tvValue.split("-")
-                    var one = splitValue[0]
-                    var two = splitValue[1]
 
-                    if(!prefix.isEmpty()){
-                        one = prefix+one
-                    }
-                    tvInput.text = (one.toDouble()-two.toDouble()).toString()
+                if(tvValue.contains("-")){
+                    dealWithOperator(tvValue, prefix, "-")
                 }
+                else if (tvValue.contains("+")){
+                    dealWithOperator(tvValue, prefix, "+")
+                }
+                else if (tvValue.contains("*")){
+                    dealWithOperator(tvValue, prefix, "*")
+                }
+                else if (tvValue.contains("/")){
+                    dealWithOperator(tvValue, prefix, "/")
+                }
+
+
             }
             catch(e:ArithmeticException){
                 e.printStackTrace()
